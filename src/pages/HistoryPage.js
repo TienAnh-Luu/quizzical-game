3c90fa1f14bda4d57bfeda86c0db562506472a03
TienAnh-Luu <81581431+TienAnh-Luu@users.noreply.github.com>
2023-02-13 20:26:36 +0700

    historypage

--- src/pages/HistoryPage.js
+++ src/pages/HistoryPage.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import HistoryDetails from '../components/HistoryDetails';
 import {Link} from 'react-router-dom';
 
 export default function HistoryPage() {
+  // [{name: 'name', score: 2, noQuiz: 5, date: '27/01/2023'}, {...}]
+  const history = JSON.parse(localStorage.getItem("history")) || [];
+ 
   return (
     <section className="history-page">
-      
+      {history === []? 
+        (<img src="../images/illustatus.svg" alt="error page" className='error-page' />)
+        :
+        history.map(hItem => (
+          <Link className='hd-link'>
+            <HistoryDetails 
+              name={hItem.name}
+              score={hItem.score}
+              noQuiz={hItem.noQuiz}
+              date={hItem.date} />
+          </Link>
+        ))
+      }
     </section>
   );
 }
